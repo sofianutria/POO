0a05ec4e51d4340d4b4e2337acafb090881e0046
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-05-30 17:18:05 +0200

    feat: add initial definition of character class

--- src/main/java/got/Character.java
+++ src/main/java/got/Character.java
@@ -1,4 +1,11 @@
 package got;
 
 public class Character {
+    private String name;
+    private String surname;
 
+    public Character(String name, String surname) {
+        this.name=name;
+        this.surname=surname;
+    }
+}
